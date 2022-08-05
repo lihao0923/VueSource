74f260b943f891a75aca7ee1c4daaac5f37e8c86
Peter Li <peterli@PeterdeMac-mini.local>
2022-08-06 02:28:56 +0800

    编写ast代码

--- src/ast/index.js
+++ src/ast/index.js
@@ -1,5 +1,18 @@
 console.log('ast!');
 
+// 试编写"智能重复"smartRepeat函数，实现：
+// 将3[abc]变为abcabcabc
+// 将3[2[a]2[b]]变为aabbaabbaabb
+// 将2[1[a]3[b]2[3[c]4[d]]]变为abbbcccddddcccddddabbbcccddddcccdddd
+
+// 不用考虑输入字符串是非法的情况，比如：
+// 2[a3[b]]是错误的，应该补一个1，即2[1[a]3[b]]
+// [abc]是错误的，应该补一个1，即1[abc]
+
+
+
+
+
 
 
 
